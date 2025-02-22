5421abca8acdad36f494c923b5dc01d4a5077833
Matt Home <matthome@Matts-MacBook-Pro.local>
2025-02-22 22:21:46 +0000

    Create Wind. func

--- gol.c
+++ gol.c
@@ -4,57 +4,55 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
-const char *WINDOW_TITLE = "Game of Life";
-const int WIDTH = 900;
-const int HEIGHT = 600;
-const Uint32 DELAY = 10000;
-const Uint32 COLOUR_WHITE = 0Xffffffff;
-
-//int drawGrid(SDL_Surface *surface, int WIDTH, int HEIGHT);
-
-int drawGrid (SDL_Surface *surface, int WIDTH, int HEIGHT) {
-    /* Draw horizontal grid*/
-    for (int i = 0; i <= WIDTH; i++) {
-        for (int j = 0; j <= HEIGHT; j += 15) {
-            SDL_Rect rect = (SDL_Rect){i, j, 1, 1};
-            SDL_FillRect(surface, &rect, COLOUR_WHITE);
-        }
-    }
-
-    /* Draw vertical grid*/
-    for (int i = 0; i <= HEIGHT; i++) {
-        for (int j = 0; j <= WIDTH; j += 15) {
-            SDL_Rect rect = (SDL_Rect){j, i, 1, 1};
-            SDL_FillRect(surface, &rect, COLOUR_WHITE);
-        }
-    }
-    return 0;
-}
-
-int main() {
-    int *xpos;
-    int *ypos;
-
-    SDL_Event event;
-    bool quit = false;
-
-    SDL_Init(SDL_INIT_VIDEO);
+SDL_Window* createWindow(const char *WINDOW_TITLE, int WIDTH, int HEIGHT) {
     SDL_Window* window = SDL_CreateWindow (
         WINDOW_TITLE,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         WIDTH, HEIGHT, 0
-    ); 
+    );
+    return 0;
+};
 
-    SDL_Surface *surface = SDL_GetWindowSurface(window);
+int main() {
+    const int WIDTH = 900;
+    const int HEIGHT = 600;
+    const char* WINDOW_TITLE = "Game of Life";
+    const Uint32 COLOUR_WHITE = 0Xffffffff;
+    bool quit = false;
 
-    drawGrid(surface, 900, 600);
+    SDL_Event event;
 
-    /*Add a square into the grid*/
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = createWindow(WINDOW_TITLE, WIDTH, HEIGHT);
+
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+    /* Draw horizontal grid 
+    for (int i = 0; i <= WIDTH; i++) {
+        for (int j = 0; j <= HEIGHT; j += 15) {
+            SDL_Rect rect = (SDL_Rect){i, j, 1, 1};
+            SDL_FillRect(surface, &rect, COLOUR_WHITE);
+            SDL_UpdateWindowSurface(window);
+        }
+    }
+
+    Draw vertical grid 
+    for (int i = 0; i <= HEIGHT; i++) {
+        for (int j = 0; j <= WIDTH; j += 15) {
+            SDL_Rect rect = (SDL_Rect){j, i, 1, 1};
+            SDL_FillRect(surface, &rect, COLOUR_WHITE);
+            SDL_UpdateWindowSurface(window);
+        }
+    }
+*/
+    /*Add a square into the grid */
     SDL_Rect rect = (SDL_Rect){15, 15, 15, 15};
     SDL_FillRect(surface, &rect, COLOUR_WHITE);
+    SDL_UpdateWindowSurface(window);
 
-    /* loop to make window appear and quit on key press*/
+    // loop to make window appear and quit on key press
     while (!quit){
         while (SDL_PollEvent(&event)){
             if (event.type == SDL_KEYDOWN){
